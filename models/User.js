b1d446cb0aefe729edeeac2e706d5d1eb371b1b1
Ronnie95 <t_daron@yahoo.com>
2023-08-27 19:54:28 -0400

    model updates

--- models/User.js
+++ models/User.js
@@ -1,23 +1,33 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-    {
-        username: {
+    const userSchema = new mongoose.Schema(
+        {
+          username: {
             type: String,
             required: true,
-            unique: true, 
-        }, 
-        password: {
+            unique: true,
+          },
+          password: {
             type: String,
             required: true,
-        }, 
-        email: {
+          },
+          email: {
             type: String,
-            required: [true, "Please provide me with an email!"],
+            required: true,
+          }
+        },
+        {
+          timestamps: true,
+          toJSON: {
+            virtuals: true,
+            // ret is the returned Mongoose document
+            transform: (_doc, ret) => {
+              delete ret.password;
+              return ret;
+            },
+          },
         }
-    },
-    
-);
+      );
 
 // mongoose.model(<mongodb collection name>, our schema)
 const Users = mongoose.model('User', userSchema);
